1fc958240865d4977b3f869bca4c44dada7baae9
xyryc <amdanikzz@gmail.com>
2024-10-22 01:50:10 +0600

    json data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
