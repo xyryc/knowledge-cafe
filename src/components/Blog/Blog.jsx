d6c4a2f2f11c252709738308ec15052da498659c
xyryc <amdanikzz@gmail.com>
2024-10-22 03:06:59 +0600

    added blog styles

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,55 @@
 import PropTypes from "prop-types";
+import { FaRegBookmark  } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const {
+    title,
+    cover,
+    reading_time,
+    author_img,
+    author,
+    posted_date,
+    hashtags,
+  } = blog;
 
-  return <div></div>;
+  return (
+    <div className="mb-20">
+      <img
+        className="rounded-lg mb-8 w-full"
+        src={cover}
+        alt={`cover picture of title ${title}`}
+      />
+      <div className="flex justify-between mb-4">
+        <div className="flex gap-6">
+          <img
+            className="w-[60px]"
+            src={author_img}
+            alt={`image of author: ${author}`}
+          />
+          <div>
+            <h4 className="text-2xl font-bold">{author}</h4>
+            <p className="text-base font-semibold text-gray-600">
+              {posted_date}
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-2 text-gray-600 items-center">
+          <span className="text-xl font-medium">{reading_time} min read</span>
+          <button>
+            <FaRegBookmark ></FaRegBookmark >
+          </button>
+        </div>
+      </div>
+      <h2 className="text-[40px] font-bold">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx} className="text-xl font-medium mr-4 text-gray-600">
+            <a href="">#{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -11,7 +12,10 @@ const Blogs = () => {
 
   return (
     <div className="md:w-2/3">
-      <h2>Blogs: {blogs.length}</h2>
+
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
